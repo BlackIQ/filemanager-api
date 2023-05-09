67e6c265290043f3a53da89d711df217f1097311
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-05-10 00:29:50 +0330

    Sort and stuff

--- app/controllers/tree/tree.controllers.js
+++ app/controllers/tree/tree.controllers.js
@@ -3,7 +3,26 @@ import directoryTree from "directory-tree";
 export const TREE = async (req, res) => {
   const { path } = req.body;
 
-  const tree = directoryTree(path);
+  const tree = directoryTree(path, {
+    attributes: ["type"],
+    depth: 1,
+  });
+
+  tree.children.sort((a, b) => {
+    if (a.type === "directory" && b.type !== "directory") {
+      return -1;
+    } else if (a.type !== "directory" && b.type === "directory") {
+      return 1;
+    } else {
+      if (a.name.startsWith(".") && !b.name.startsWith(".")) {
+        return 1;
+      } else if (!a.name.startsWith(".") && b.name.startsWith(".")) {
+        return -1;
+      } else {
+        return a.name.localeCompare(b.name);
+      }
+    }
+  });
 
   res.status(200).send(tree);
 };
